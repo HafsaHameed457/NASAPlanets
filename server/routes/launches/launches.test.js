382a10f76ffa19112f59acd70c6ccfb27441a7d5
Hafsa Hameed <hafsa.hameed457@gmail.com>
2023-10-09 16:32:10 +0500

    changes

--- server/routes/launches/launches.test.js
+++ server/routes/launches/launches.test.js
@@ -1,14 +1,26 @@
-import { request } from "supertest";
 import app from "../../src/express";
 describe("Test GET /launches", () => {
   test("It should respond with 200 success", async () => {
-    const response = await request(app).get("/launch");
-    expect(response).toBe(200);
+    const response = await request(app)
+      .get("/launch")
+      .expect("Content-Type", /json/)
+      .expect(200);
   });
 });
 
 describe("Test POST /launches", () => {
-  test("It should respond with 200 success", () => {});
+  test("It should respond with 201 created", async () => {
+    const response = await request(app)
+      .post("/launch")
+      .send({
+        missionName: "KeplerSpace",
+        rocket: "Explorer Hafsa",
+        launchDate: "December 20 2030",
+        destination: "Kepler-42 bh",
+      })
+      .expect("Content-Type", /json/)
+      .expect(200);
+  });
   test("It should catch missing required properties", () => {});
   test("It should catch invald dates", () => {});
 });
