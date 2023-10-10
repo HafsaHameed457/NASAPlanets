0dbd1e0df543a67a5aad0c7638cde11b09974cc4
Hafsa Hameed <hafsa.hameed457@gmail.com>
2023-10-10 22:56:58 +0500

    testing module added

--- server/routes/launches/launches.test.js
+++ server/routes/launches/launches.test.js
@@ -1,4 +1,4 @@
-import app from "../../src/express";
+import { app } from "../../src/express.js";
 describe("Test GET /launches", () => {
   test("It should respond with 200 success", async () => {
     const response = await request(app)
@@ -19,7 +19,14 @@ describe("Test POST /launches", () => {
         destination: "Kepler-42 bh",
       })
       .expect("Content-Type", /json/)
-      .expect(200);
+      .expect(201);
+
+    expect(response.body).toMatchObject({
+      missionName: "KeplerSpace",
+      rocket: "Explorer Hafsa",
+      launchDate: "December 20 2030",
+      destination: "Kepler-42 bh",
+    });
   });
   test("It should catch missing required properties", () => {});
   test("It should catch invald dates", () => {});
